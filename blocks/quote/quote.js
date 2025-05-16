303029b94f94e9093491402baf17b02e862ec559
jvaishnavrp <jvaishnav@rightpoint.com>
2025-05-16 16:19:28 +0530

    tag field

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,8 +1,10 @@
 export default function decorate(block) {
     const [quoteWrapper] = block.children;
     block.classList.add(`test-class`);
-
+    
     const blockquote = document.createElement('blockquote');
     blockquote.textContent = quoteWrapper.textContent.trim();
     quoteWrapper.replaceChildren(blockquote);
+
+  
   }
